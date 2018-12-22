48c9f5eb790c25e675aacc610e323f46dcc2c525
Sam Bennrubi <sbennrubi@gmail.com>
2018-12-22 19:58:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,14 @@ class Song
   
   def self.genre_count
     genre_hash = {}
-    @@genres.map do |genre|
+    @@genres.each do |genre|
       if genre_hash.include?(genre)
         genre_hash[genre] += 1
       else
         genre_hash[genre] = 1
       end
     end
+    genre_hash
   end
 
 end
